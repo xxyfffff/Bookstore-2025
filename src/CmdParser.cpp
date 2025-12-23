48a5ce3abd51e4c1346e7ed4c21a5bfb97b5c0e4
yifei <yifei06@sjtu.edu.cn>
2025-12-23 22:15:26 +0800

    fix: check float

--- src/CmdParser.cpp
+++ src/CmdParser.cpp
@@ -76,7 +76,7 @@ static bool isValidQuantity(const std::string &s) {
     // 不能有前导 0
     if (s.size() > 1 && s[0] == '0') return false;
 
-    for (char c : s)
+    for (char c: s)
         if (!std::isdigit(c)) return false;
 
     try {
@@ -90,36 +90,29 @@ static bool isValidQuantity(const std::string &s) {
 static bool isValidPrice(const std::string &s) {
     if (s.empty()) return false;
 
-    bool dot = false;
+    int n = s.size();
     int dotPos = -1;
 
-    for (int i = 0; i < (int)s.size(); i++) {
+    for (int i = 0; i < n; ++i) {
         char c = s[i];
         if (c == '.') {
-            if (dot) return false;   // 多个点
-            dot = true;
+            if (dotPos != -1) return false; // 多个 .
             dotPos = i;
         } else if (!std::isdigit(c)) {
             return false;
         }
     }
 
-    if (dot) {
-        if (dotPos == 0 || dotPos == (int)s.size() - 1) {
-            return false;
-        }
+    // 不能是 ".1" 或 "1."
+    if (dotPos == 0 || dotPos == n - 1)
+        return false;
 
-        if ((int)s.size() - dotPos - 1 > 2) {
-            return false;
-        }
-    }
+    // 小数位数不超过 2
+    if (dotPos != -1 && n - dotPos - 1 > 2)
+        return false;
 
-
-    if (s[0] == '0') {
-        if (s.size() > 1 && s[1] != '.') {
-            return false;
-        }
-    }
+    if (s[0] == '0' && n > 1 && s[1] != '.')
+        return false;
 
     try {
         double v = std::stod(s);
@@ -128,6 +121,8 @@ static bool isValidPrice(const std::string &s) {
         return false;
     }
 }
+
+
 
 std::vector<std::string> CmdParser::tokenize(const std::string &line) {
     std::vector<std::string> res;
@@ -208,9 +203,9 @@ bool CmdParser::validate(const std::vector<std::string> &t, CommandType type) {
             //      isValidUserID(t[2]) <<
             //       isValidUsername(t[3]);
             return (n == 4 &&
-                   isValidUserID(t[1]) &&
-                   isValidUserID(t[2]) &&
-                   isValidUsername(t[3]));
+                    isValidUserID(t[1]) &&
+                    isValidUserID(t[2]) &&
+                    isValidUsername(t[3]));
 
         case CommandType::SU:
             if (n == 2) {
